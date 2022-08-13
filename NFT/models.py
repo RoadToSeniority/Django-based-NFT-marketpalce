9db2e84056e893a85db8e82a6ca94ba7c20382da
Ida Taaki <47431356+idataaki@users.noreply.github.com>
2022-08-14 00:26:46 +0430

    NFTapp

--- NFT/models.py
+++ NFT/models.py
@@ -1,17 +1,21 @@
 from unicodedata import name
 from django.db import models
 from enum import Enum
-
-class TypeNFT(Enum):
-    Picture, Voice, Gif = 1, 2, 3
     
 class NFT(models.Model):
+
+    class NFTType(models.TextChoices):
+        Picture = 'Picture'
+        Voice = 'Voice'
+        Gif = 'Gif'
+        
     name = models.CharField(max_length=100)
     IpfsAddress = models.CharField(unique=True, max_length=100)
-    UpdatedAt = models.DateTimeField()
-    CreatedAt = models.DateTimeField()
-    LastPrice = models.IntegerField()
+    NftType = models.CharField(max_length=100, choices=NFTType.choices)
+    UpdatedAt = models.DateTimeField(auto_now=True)
+    CreatedAt = models.DateTimeField(auto_now_add=True)
+    LastPrice = models.DecimalField(max_digits=6, decimal_places=2)
 
 class Price(models.Model):
-    IRL = models.FloatField(max_length=100)
-    USD = models.FloatField(max_length=100)
+    IRL = models.DecimalField(max_digits=6, decimal_places=2)
+    USD = models.DecimalField(max_digits=6, decimal_places=2)

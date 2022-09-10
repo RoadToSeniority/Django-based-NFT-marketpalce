1c6c26e6944c5e25925c68fb37ae8a73d1b9dd50
Work <nfilsaraee@gmail.com>
2022-09-10 19:57:54 +0430

    Added Urls and biolerplate for user views

---
+++ user/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from .views import (
+    login_form,
+    recieving_sth_from_browser,
+    sending_sth_to_browser,
+    saying_hi_to_browser,
+)
+
+urlpatterns = [
+    path("hi/", saying_hi_to_browser),
+    path("get/", sending_sth_to_browser),
+    path("post/", recieving_sth_from_browser),
+    path("login_form/", saying_hi_to_browser),
+]

1e7c8271c27dfae8a68953a976e088598ea22ea9
Work <nfilsaraee@gmail.com>
2022-09-10 20:00:16 +0430

    Added type hint to request

--- user/views.py
+++ user/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render, HttpResponse
+from django.http import HttpRequest
 
 
-def login_form(request):
+def login_form(request: HttpRequest):
     ...
 
 
-def recieving_sth_from_browser(request):
+def recieving_sth_from_browser(request: HttpRequest):
     ...
 
 
-def sending_sth_to_browser(request):
+def sending_sth_to_browser(request: HttpRequest):
     ...
 
 
-def saying_hi_to_browser(request):
+def saying_hi_to_browser(request: HttpRequest):
     return HttpResponse("HI ^^")
